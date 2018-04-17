a36849692dacfb2f65ef6dbf5bbc7200bf6dcff0
Robert Walsh <rw539@bath.ac.uk>
2018-04-17 23:42:50 +0100

    Up until "Middleware to match environment"

---
+++ Project001/IGreeter.cs
@@ -0,0 +1,11 @@
+public interface IGreeter{
+    string GetMessageOfTheDay();
+    
+}
+
+public class Greeter : IGreeter{
+
+    public string GetMessageOfTheDay(){
+        return "Greetings!";
+    }
+}
