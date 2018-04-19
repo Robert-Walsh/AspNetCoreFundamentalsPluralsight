023e9a417554554609c7dd2c521184649424748e
Robert Walsh <robert_walsh_8@hotmail.com>
2018-04-19 16:30:14 +0100

    Conventional Routes

---
+++ Project001/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project001.Controllers
+{
+    public class AboutController
+    {
+
+        public string Phone()
+        {
+            return "07891632506";
+        }
+
+        public string Address()
+        {
+            return "25 Mallard Way, Watford, UK";
+        }
+    }
+}
